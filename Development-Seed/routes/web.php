7cfdc6035558d9f71643b13aa626add36c276a68
Alij <nhsp0480@gmail.com>
2025-12-27 00:26:20 +0200

    adding api's

--- Development-Seed/routes/web.php
+++ Development-Seed/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -15,5 +16,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+
 
 require __DIR__.'/settings.php';
