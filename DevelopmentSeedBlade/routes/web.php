e094da2d4dcf27e142db37e06e72e445439c0491
Alij <nhsp0480@gmail.com>
2025-12-31 18:15:22 +0200

    removing /about and /contact routes

--- DevelopmentSeedBlade/routes/web.php
+++ DevelopmentSeedBlade/routes/web.php
@@ -19,9 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 // Home / Landing Page
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/about', [HomeController::class, 'about'])->name('about');
-Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
-
 /*
 |--------------------------------------------------------------------------
 | Authenticated User Routes
